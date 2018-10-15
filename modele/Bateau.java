35409a590a41cde73373e9ba483b5aea9d2cd3c6
jetrivisonno <44096383+jetrivisonno@users.noreply.github.com>
2018-10-14 21:16:21 -0400

    Update Bateau.java

--- modele/Bateau.java
+++ modele/Bateau.java
@@ -1,5 +1,16 @@
 package modele;
 
-public class Bateau {
+import java.lang.reflect.Array;
 
+public class Bateau {
+	private String nom;
+	private int unites;
+	private Array[] position;
+	
+	public Bateau(String nom, int unites) {
+		Bateau bateau1 = new Bateau("bateau patrouille", 2);
+		Bateau bateau2 = new Bateau("sous-marin", 3);
+		Bateau bateau3 = new Bateau("destroyer", 3);
+		Bateau bateau4 = new Bateau("porte avion", 4);
+	}
 }
