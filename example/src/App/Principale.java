b56dfee82c1214f8a4837211065fd44bf400aefb
garbage bag <44094726+anActualGarbageBag@users.noreply.github.com>
2018-10-18 12:19:28 -0400

    less bukkake version of the tp

--- example/src/App/Principale.java
+++ example/src/App/Principale.java
@@ -1,11 +1,13 @@
 package App;
 
 import Vue.InterfaceGraphiqueBattleship;
+import Modele.*;
 
 public class Principale {
 
 	public static void main(String[] args) {
 		new InterfaceGraphiqueBattleship();
+		System.out.println();
 	}
 
 }
